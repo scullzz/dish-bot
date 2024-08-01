c22d464754cb900c4f2f97b1a15e76b7b2be960a
dream1ike <120911873+dream1ike@users.noreply.github.com>
2024-08-02 01:38:21 +0500

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,13 @@ import Order from "./components/order/Order";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import Main from "./components/main/Main";
+import App from "./components/App/App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router>
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/ogo" element={<Main />}></Route>
         <Route path="/order" element={<Order />}></Route>
       </Routes>
     </Router>
