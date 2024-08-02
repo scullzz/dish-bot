48d7c7a27b9fcf409d1f977a2b39b8ec793a6758
mgwr <aaskarov870@gmail.com>
2024-08-02 14:24:11 +0500

    shit

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -72,7 +72,7 @@ router.post('/', async (req, res) => {
   try {
     const order = await prisma.order.create({
       data: {
-        tg: parseInt(user_id),
+        tgUserId: parseInt(user_id),
         status: 'processing',
         locationUrl: locationUrl,
         phoneNumber: phoneNumber,
