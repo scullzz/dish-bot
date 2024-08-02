eb02a8bf4da71372e2c4722b04e531c0a8b427f0
mgwr <aaskarov870@gmail.com>
2024-08-02 15:38:44 +0500

    shit

--- client/src/components/order/Order.jsx
+++ client/src/components/order/Order.jsx
@@ -49,7 +49,7 @@ const Order = () => {
   const MakeOrder = async () => {
         try {
           const orderData = {
-            user_id: parseInt(userData?.id),
+            user_id: userData?.id,
             items: orderList,
             locationUrl: "blabla",
             phoneNumber: "123123123",
