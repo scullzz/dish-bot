96ba53a42b3c4196a145b709b767098867640362
mgwr <aaskarov870@gmail.com>
2024-08-03 17:32:33 +0500

    index

--- server/index.js
+++ server/index.js
@@ -50,6 +50,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/categories', require('./routes/categories'));
+app.use('/api/user_info', require('./routes/userinfo'));
 
 // Настройка Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
