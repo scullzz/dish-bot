8c1424de8835eeb1dc77b6ef456773096c3fc654
mgwr <aaskarov870@gmail.com>
2024-08-03 17:28:29 +0500

    userinfo

---
+++ server/routes/userinfo.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const prisma = require('../prisma');
+
+router.get('/:user_id', async (req, res) => {
+    const { user_id } = req.params;
+    try {
+        const user_info = await prisma.user.findFirst({
+            where: { telegramId: BigInt(user_id) }
+        });
+        res.json(user_info);
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
